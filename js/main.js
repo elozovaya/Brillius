ada1645af55a994fa9da602b10815212f8f27b41
elozovaya <lozovayakat@gmail.com>
2016-09-17 21:20:50 +0300

    pixel perfect

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('.carousel').slick({
+	$('#carousel').slick({
 		infinite: true,
 		dots: false,
 		fade: true,
@@ -55,7 +55,7 @@ function initMobileNav(){
 	// Knob
 	$(function($) {
 		$(".knob").knob({
-			 change : function (value) {
+			change : function (value) {
 				  //console.log("change : " + value);
 			},
 			release : function (value) {
@@ -71,37 +71,37 @@ function initMobileNav(){
 			draw : function () {
 
 				  // "tron" case
-				if(this.$.data('skin') == 'tron') {
+				// if(this.$.data('skin') == 'tron') {
 
-					this.cursorExt = 0.3;
+				// 	this.cursorExt = 0.3;
 
-					var a = this.arc(this.cv)  // Arc
-							  , pa                   // Previous arc
-							  , r = 1;
+				// 	var a = this.arc(this.cv)  // Arc
+				// 			  , pa                   // Previous arc
+				// 			  , r = 1;
 
-					this.g.lineWidth = this.lineWidth;
+				// 	this.g.lineWidth = this.lineWidth;
 
-					if (this.o.displayPrevious) {
-						 pa = this.arc(this.v);
-						 this.g.beginPath();
-						 this.g.strokeStyle = this.pColor;
-						 this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, pa.s, pa.e, pa.d);
-						 this.g.stroke();
-					}
+				// 	if (this.o.displayPrevious) {
+				// 		 pa = this.arc(this.v);
+				// 		 this.g.beginPath();
+				// 		 this.g.strokeStyle = this.pColor;
+				// 		 this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, pa.s, pa.e, pa.d);
+				// 		 this.g.stroke();
+				// 	}
 
-					this.g.beginPath();
-					this.g.strokeStyle = r ? this.o.fgColor : this.fgColor ;
-					this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, a.s, a.e, a.d);
-					this.g.stroke();
+				// 	this.g.beginPath();
+				// 	this.g.strokeStyle = r ? this.o.fgColor : this.fgColor ;
+				// 	this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, a.s, a.e, a.d);
+				// 	this.g.stroke();
 
-					this.g.lineWidth = 2;
-					this.g.beginPath();
-					this.g.strokeStyle = this.o.fgColor;
-					this.g.arc( this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
-					this.g.stroke();
+				// 	this.g.lineWidth = 7;
+				// 	this.g.beginPath();
+				// 	this.g.strokeStyle = this.o.fgColor;
+				// 	this.g.arc( this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
+				// 	this.g.stroke();
 
-					return false;
-				}
+				// 	return false;
+				// }
 			}
 		});
 	});
